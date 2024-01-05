83fb7247478c5b418309e86179112a87f3fc4969
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-01-05 14:53:23 -0300

    Updates

---
+++ _next/static/A6KFjXfk3Tg2BbwpFGvQF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/117-0623932900584172.js","static/chunks/pages/index-f9f7b4e6e7e39c5c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
